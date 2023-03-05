8ecbe69bb53a479a1ffe1f7a1adb5163faaa4da9
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-05 20:34:58 -0300

    feat(inventory): implement read xml file

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,21 +1,45 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     @staticmethod
+    def read_csv(path):
+        with open(path) as csv_file:
+            reader = csv.DictReader(csv_file)
+            return list(reader)
+
+    @staticmethod
+    def read_json(path):
+        with open(path) as json_file:
+            content = json_file.read()
+            return json.loads(content)
+
+    @staticmethod
+    def read_xml(path):
+        data = []
+        tree = ET.parse(path)
+        root = tree.getroot()
+        for record in root.findall("record"):
+            item = {}
+            for header in record:
+                print(header)
+                item[header.tag] = header.text
+            data.append(item)
+        return data
+
+    @staticmethod
     def read_data(path):
         data = []
-        if path.endswith('.csv'):
-            with open(path) as csv_file:
-                reader = csv.DictReader(csv_file)
-                data = list(reader)
-        elif path.endswith('.json'):
-            with open(path) as json_file:
-                content = json_file.read()
-                data = json.loads(content)
+        if path.endswith(".csv"):
+            data = Inventory.read_csv(path)
+        elif path.endswith(".json"):
+            data = Inventory.read_json(path)
+        elif path.endswith(".xml"):
+            data = Inventory.read_xml(path)
         return data
 
     @staticmethod
