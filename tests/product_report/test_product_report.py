155a100dccdf9ccabda6eefc5d4600a31ad8f351
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-06 14:00:13 -0300

    test: product_report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+product = Product(
+    4,
+    "Café da Trybe",
+    "Trybe",
+    "01/01/2022",
+    "01/02/2022",
+    "69420",
+    "em local seco",
+)
+
+expected = "O produto Café da Trybe fabricado em 01/01/2022 por Trybe com validade até 01/02/2022 precisa ser armazenado em local seco."
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    assert str(product) == expected
