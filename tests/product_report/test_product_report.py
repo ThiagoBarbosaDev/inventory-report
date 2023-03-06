8ab02f927c50e9e19e20174330c82c2a990e239f
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-03-06 14:49:42 -0300

    test(test_relatorio_produto): move arrange to test scope

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,18 +1,19 @@
 from inventory_report.inventory.product import Product
 
-product = Product(
-    4,
-    "Café da Trybe",
-    "Trybe",
-    "01/01/2022",
-    "01/02/2022",
-    "69420",
-    "em local seco",
-)
-
-expected = "O produto Café da Trybe fabricado em 01/01/2022 por Trybe com \
-validade até 01/02/2022 precisa ser armazenado em local seco."
-
 
 def test_relatorio_produto():
+    product = Product(
+        4,
+        "Café da Trybe",
+        "Trybe",
+        "2022-01-01",
+        "2022-04-04",
+        "69420",
+        "em local seco",
+    )
+
+    expected = (
+        "O produto Café da Trybe fabricado em 2022-01-01 por Trybe com "
+        "validade até 2022-04-04 precisa ser armazenado em local seco."
+        )
     assert str(product) == expected
